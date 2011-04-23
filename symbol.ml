1fa3b9f9bc3dfbd7f4f1161ab36ded80835b25cb
Andrei Formiga <archimedes_siracusa@hotmail.com>
2011-04-23 16:59:59 -0300

    Changed symbol implementation to improve comparison (symbols for the same string must have the same representation).

--- symbol.ml
+++ symbol.ml
@@ -8,33 +8,18 @@
  *)
 
 (*** type definitions ***)
-type symbol = int
+type symbol = (string * int)
 
 (*** data ****)
 
-(** Current index for symbols *)
-let index = ref 0
-
-let start_size = 30
-
-(** The symbol table *)
-let sym_table : (symbol, string) Hashtbl.t = Hashtbl.create start_size
-
-(*** functions to operate with symbols ***)
-
 let create str = 
-  let i = !index in
-  Hashtbl.add sym_table i str;
-  incr index;
-  i
+  (str, Hashtbl.hash str)
 
-let str sym = 
-  if sym >= !index then 
-    failwith "Symbol not created"
-  else
-    Hashtbl.find sym_table sym
+let str (s, h) = 
+  s
 
-let compare s1 s2 = compare s1 s2 
+let compare (s1,h1) (s2,h2) = compare h1 h2
+
 
   
 
